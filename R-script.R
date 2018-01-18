e1445df1b2f3a787514d5193bf97f0687ddf0779
marcus-seldin <32966149+marcus-seldin@users.noreply.github.com>
2018-01-18 15:23:17 -0800

    Update R-script.R

--- R-script.R
+++ R-script.R
@@ -1,5 +1,5 @@
 #Note: This example performs QENIE on the liver-to-adipose circuit. 
-#Preproccesing of data from the original study is listed in the README file 
+#Preproccesing of data from the original study (GSE64770) is listed in the README.md file 
 #Import your data
 Adipose <- read.delim("adipose.txt", header = T)
 Liver <- read.delim("liver.txt", header = T)
